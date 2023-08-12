e88d3ffd1741704f999734e9dc0bed898c3f465a
Dennis Demir <dennisjdemir@gmail.com>
2023-08-12 03:10:29 -0400

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
-def create_table(base):
+def create_table(base, engine):
+    base.metadata.create_all(engine)
     pass
 
 def save(session, dog):
+    session.add(dog)
     pass
 
 def get_all(session):
+    return session.query(Dog).all()
     pass
 
 def find_by_name(session, name):
+    return session.query(Dog).filter(Dog.name == name).first()
     pass
 
 def find_by_id(session, id):
+    return session.query(Dog).filter(Dog.id == id).first()
     pass
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
     pass
 
 def update_breed(session, dog, breed):
+    return session.query(Dog).update({
+        Dog.breed: breed
+    })
     pass
